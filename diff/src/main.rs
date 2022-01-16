61be67b55c2bedca6df09aa58721e589c9786fd6
Martin Lux <martin.lux@deepl.com>
2022-01-16 15:44:58 +0100

    test chapter

--- diff/src/main.rs
+++ diff/src/main.rs
@@ -1,4 +1,4 @@
-use similar::{TextDiff};
+use similar::{TextDiff, ChangeTag};
 
 extern crate serde;
 
@@ -32,13 +32,35 @@ fn main() {
 
     #[derive(Serialize, Deserialize, Debug)]
     struct JsonData<'a> {
-        value: &'a str
+        value: &'a str,
+        tag: &'a str,
+        old_index: Option<usize>,
+        new_index: Option<usize>,
     }
-    
-    let d = JsonData {
-        value: all_changes[0].value()
-    };
 
-    println!("{:?}", serde_json::to_string(&d));
+    // let d = JsonData {
+    //     value: all_changes[0].value(),
+    //     tag: match all_changes[0].tag() {
+    //         ChangeTag::Delete => "delete",
+    //         ChangeTag::Insert => "add",
+    //         ChangeTag::Equal => "equal", 
+    //     },
+    //     old_index: all_changes[0].old_index(),
+    //     new_index: all_changes[0].new_index()
+    // };
+
+    let json_array: Vec<_> = all_changes.iter().map(|v| JsonData {
+        value: v.value(),
+        tag: match v.tag() {
+            ChangeTag::Delete => "delete",
+            ChangeTag::Insert => "add",
+            ChangeTag::Equal => "equal", 
+        },
+        old_index: v.old_index(),
+        new_index: v.new_index()
+    }).collect();
+
+    println!("{:?}", serde_json::to_string(&json_array));
+    //println!("{:?}", all_changes);
 
 }
